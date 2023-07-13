21c7208f2a322aa66b2bf2e25f5712a711f6a715
leticiaocr <leticiabritooliveirac@gmail.com>
2023-07-13 19:35:37 -0300

    feature: finalizando projeto

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro){
-            throw Error ('Esse cep não existe')
+async function buscaEndereco(cep) {
+    try {
+        var mensagemErro = document.getElementById('erro');
+        mensagemErro.innerHTML = "";
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error('CEP não existente!');
         }
-        console.log(r)})
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído'));
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
 
-console.log(consultaCep);
+        return consultaCepConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p> CEP inválido, tente novamente`
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+
+
+
+
+
+
+
+
+
+
